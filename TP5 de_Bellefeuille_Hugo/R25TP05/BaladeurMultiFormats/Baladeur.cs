c14522e38aeb54ac3d976199c65ee65ac56125ec
Hugo-dB <6295691@cegepmontpetit.ca>
2025-05-12 13:58:38 -0400

    FCT : TODO #08 Ajout de la classe Baladeur

---
+++ TP5 de_Bellefeuille_Hugo/R25TP05/BaladeurMultiFormats/Baladeur.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace BaladeurMultiFormats
+{
+    public class Baladeur : IBaladeur
+    {
+        #region CONSTANTES
+
+        /// <summary>
+        /// Nom du répertoire des chansons
+        /// </summary>
+        private const string NOM_RÉPERTOIRE = "Chansons";
+
+        #endregion
+
+        #region CHAMPS ET PROPRIÉTÉS
+
+        /// <summary>
+        /// Liste des chansons
+        /// </summary>
+        private List<Chanson> m_colChansons;
+
+        /// <summary>
+        /// Obtient le nombre de chansons.
+        /// </summary>
+        public int NbChansons { get { return m_colChansons.Count; } }
+
+        #endregion
+
+        #region CONSTRUCTEUR
+
+        public Baladeur()
+        {
+            ConstruireLaListeDesChansons();
+        }
+
+        #endregion
+
+        #region MÉTHODES
+
+        public void AfficherLesChansons(ListView pListView)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Chanson ChansonAt(int pIndex)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void ConstruireLaListeDesChansons()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void ConvertirVersAAC(int pIndex)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void ConvertirVersMP3(int pIndex)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void ConvertirVersWMA(int pIndex)
+        {
+            throw new NotImplementedException();
+        }
+
+        #endregion
+    }
+}
